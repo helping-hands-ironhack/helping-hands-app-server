aa6116f04dfbb7ec1da8aacadaaae0abbf2ac6d8
Marcelus <kz_@hotmail.es>
2022-03-10 11:38:58 +0100

    models and first routes

---
+++ routes/pax.routes.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const router = express.Router();
+const Pax = require("../models/Pax.model");
+
+router.post("/", (req, res) => {
+    Pax.create(req.body)
+        .then(newPax => res.json(newPax))
+        .catch((error) => console.log(error))
+});
+
+router.get("/:paxId", (req, res) => {
+    Pax.findById(params.paxId)
+        .then(pax => res.json(pax))
+        .catch((error) => console.log(error))
+});
+
+router.put("/:paxId", (req, res) => {
+    Pax.findByIdAndUpdate(req.body, params.paxId)
+        .then(updatedPax => res.json(updatedPax))
+        .catch((error) => console.log(error))
+});
+
+router.delete("/:paxId", (req, res) => {
+    Pax.findByIdAndDelete(req.params.paxId)
+        .then((deletedPax) => res.json(deletedPax))
+        .catch((error) => console.log(error))
+});
