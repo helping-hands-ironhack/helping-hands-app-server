1f77aad7a3a514a2f6efcf398201a85097826879
Aleix Abulí <aleixabuli088@gmail.com>
2022-03-10 13:41:05 +0100

    jeje

--- db/index.js
+++ db/index.js
@@ -6,11 +6,7 @@ const mongoose = require("mongoose");
 // ℹ️ Sets the MongoDB URI for our app to have access to it.
 // If no env has been set, we dynamically set it to whatever the folder name was upon the creation of the app
 
-<<<<<<< HEAD
-const MONGO_URI = process.env.MONGODB_URI
-=======
 const MONGO_URI = process.env.MONGODB_URI;
->>>>>>> cc9f0962921ff75a5fab03c20a58da03409fdc9b
 
 mongoose
   .connect(MONGO_URI, {
