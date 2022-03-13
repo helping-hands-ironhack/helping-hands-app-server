bac43677c43ea5244a8d060e7706ac5c15e3d4f8
Twalinzor <twalin.rift@gmail.com>
2022-03-13 12:14:59 +0100

    Bug fixed

--- models/Accommodation.model.js
+++ models/Accommodation.model.js
@@ -1,15 +1,18 @@
 const { Schema, model } = require("mongoose");
 
 const accommodationSchema = new Schema(
-    {
-        capacity: { type: Number, required: true },
-        rooms: { type: Number, required: true },
-        description: { type: String, required: true },
-        owner: { type: Schema.Types.ObjectId, ref: 'User' },
-        resquests: [{ type: Schema.Types.ObjectId, ref: 'Pax' }],
-        isHosting: { type: Boolean, default: false },
-        currentGuests: [{ type: Schema.Types.ObjectId, ref: 'Pax' }],
-        picture: {type:String, default: "https://cdn.pixabay.com/photo/2017/07/11/00/24/house-2492054_960_720.png"}
+  {
+    capacity: { type: Number, required: true },
+    rooms: { type: Number, required: true },
+    description: { type: String, required: true },
+    owner: { type: Schema.Types.ObjectId, ref: "User" },
+    resquests: [{ type: Schema.Types.ObjectId, ref: "Pax" }],
+    isHosting: { type: Boolean, default: false },
+    currentGuests: [{ type: Schema.Types.ObjectId, ref: "Pax" }],
+    picture: {
+      type: String,
+      default:
+        "https://cdn.pixabay.com/photo/2017/07/11/00/24/house-2492054_960_720.png",
     },
   },
   {
